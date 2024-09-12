18f3fc4b0fdbd15fd4cde1f0fddabebe412551c8
archdroid20 <72358727+archdroid20@users.noreply.github.com>
2024-09-11 18:59:00 -0700

    Update custom.el
    Just a temp file

--- custom.el
+++ custom.el
@@ -1,1 +1,17 @@
 
+(defun print-doom-emacs-keybindings ()
+  "Print all keybindings for Doom Emacs."
+  (interactive)
+  (with-output-to-temp-buffer "*Doom Emacs Keybindings*"
+    (dolist (map (list evil-normal-state-map
+                       evil-insert-state-map
+                       evil-visual-state-map
+                       evil-motion-state-map))
+      (let ((keymap (copy-keymap map)))
+        (map-keymap
+         (lambda (key binding)
+           (when (commandp binding)
+             (princ (format "%s -> %s\n"
+                            (key-description (vector key))
+                            binding))))
+         keymap)))))
